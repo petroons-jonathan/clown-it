a63cbda27fc9c8498ef683bf1231f8c183aee053
petroons-jonathan <petroons.jonathan@gmail.com>
2022-06-18 10:31:40 +0200

    Change structure

--- script/script.js
+++ script/script.js
@@ -1,36 +1,42 @@
+
+// Get the button in variable
 let klaxon = document.getElementById("klaxon");
 let tambour = document.getElementById("tambour");
 let oogah = document.getElementById("oogah");
 let rire = document.getElementById("rire");
 
-klaxon.addEventListener("click", playKlaxon);
 
+// Get the music in variable
+const soundKlaxon = new Audio("media/sounds/klaxon.wav");
+const soundTambour = new Audio("media/sounds/tambour.wav");
+const soundoogah = new Audio("media/sounds/oogah.wav");
+const soundRire = new Audio("media/sounds/rire.wav");
+
+// Add event on click
+klaxon.addEventListener("click", playKlaxon);
+tambour.addEventListener("click", playTambour);
+oogah.addEventListener("click", playOogah);
+rire.addEventListener("click", playRire);
+
+
+// Function
 function playKlaxon() {
-    const soundKlaxon = new Audio("media/sounds/klaxon.wav");
     soundKlaxon.play()
     console.log("Je suis un klaxon");
 }
 
-tambour.addEventListener("click", playTambour);
-
 function playTambour() {
-    const soundTambour = new Audio("media/sounds/tambour.wav");
     soundTambour.play()
     console.log("Je suis un tambour");
 }
 
-oogah.addEventListener("click", playOogah);
-
 function playOogah() {
-    const soundoogah = new Audio("media/sounds/oogah.wav");
+
     soundoogah.play()
     console.log("Je suis un oogah");
 }
 
-rire.addEventListener("click", playRire);
-
 function playRire() {
-    const soundRire = new Audio("media/sounds/rire.wav");
     soundRire.play()
     console.log("Je suis un rire");
 }
